f4e2221db9c7e634d52f87f2586b847d40a8d5c5
Takashi Ogura <t.ogura@gmail.com>
2017-08-13 17:51:56 +0900

    Add test

---
+++ tests/test_link_tree.rs
@@ -0,0 +1,23 @@
+extern crate alga;
+extern crate nalgebra as na;
+extern crate k;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    pub fn test_tree() {
+        let tree = k::urdf::create_tree_from_file::<f32, _>("urdf/sample.urdf").unwrap();
+        assert_eq!(tree.dof(), 12);
+        let all_names = tree.get_all_joint_names();
+        assert!(all_names.len() == 13);
+        println!("{}", all_names[0]);
+        assert!(all_names[0] == "root");
+        assert!(all_names[1] == "l_shoulder_yaw");
+
+        let names = tree.get_joint_names();
+        assert!(names.len() == 12);
+        println!("{}", names[0]);
+        assert!(names[0] == "l_shoulder_yaw");
+    }
+}
