1a3decbbff6874f10a34cafa8efe42c4510cd4e4
Takashi Ogura <t.ogura@gmail.com>
2019-01-13 08:58:35 +0900

    Update bench

--- benches/fk.rs
+++ benches/fk.rs
@@ -25,6 +25,14 @@ test bench_rctree            ... bench:       2,203 ns/iter (+/- 12)
 test bench_rctree_set_joints ... bench:          25 ns/iter (+/- 0)
 test bench_rctree_ik ... bench:      20,425 ns/iter (+/- 139)
 ```
+
+## v0.12.1 on MacBook Pro (Retina, 13-inch, Early 2015, 2.9GHz - 16GB)
+
+```
+test bench_rctree            ... bench:       3,107 ns/iter (+/- 2,837)
+test bench_rctree_set_joints ... bench:         296 ns/iter (+/- 30)
+test bench_rctree_ik ... bench:      10,622 ns/iter (+/- 1,203)
+```
 */
 
 fn generate_random_joint_angles_from_limits<T>(limits: &Vec<Option<k::joint::Range<T>>>) -> Vec<T>
@@ -55,10 +63,7 @@ fn bench_rctree(b: &mut test::Bencher) {
 #[bench]
 fn bench_rctree_set_joints(b: &mut test::Bencher) {
     let chain = k::Chain::<f64>::from_urdf_file("urdf/sample.urdf").unwrap();
-    let limits = chain
-        .iter_joints()
-        .map(|j| j.joint().limits.clone())
-        .collect();
+    let limits = chain.iter_joints().map(|j| j.limits.clone()).collect();
     let angles = generate_random_joint_angles_from_limits(&limits);
     b.iter(|| {
         chain.set_joint_positions(&angles).unwrap();
