2c234eb5e4a8a439c4c37545cc9ea9dfac73698d
Ben Berman <ben.m.berman@gmail.com>
2019-07-11 10:19:27 -0400

    Fix a small handful of typos and odd formatting/grammar in docs

--- src/prelude.rs
+++ src/prelude.rs
@@ -13,5 +13,5 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-//! Load basic trais of `k`
+//! Load basic traits of `k`
 pub use ik::InverseKinematicsSolver;
